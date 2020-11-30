10b2ca692b335be48266fc2ff64c6151f2413277
Violeta <violetapopans@gmail.com>
2020-11-30 12:33:49 +0100

    Add project files.

---
+++ Vezba25/FileMenagment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Vezba25
+{
+    class FileMenagment
+    {
+        private static string FileName = @"C:\Kurs\cas25_zadatakd.txt";
+        public static void WriteList(string InputData)
+        {
+            using (StreamWriter FileHandle = new StreamWriter(FileName, true))
+            {
+                FileHandle.WriteLine("{0}", string.Join(";", InputData));
+            }
+        }
+        public static List<string> Readlist()
+        {
+            List<string> ListOfNames = new List<string>();
+            using (StreamReader FileHandle = new StreamReader(FileName))
+            {
+                string FileContents = "";
+                while((FileContents = FileHandle.ReadLine()) != null)
+                {
+                    ListOfNames.Add(FileContents);
+                }
+
+            }
+            return ListOfNames;
+        }  
+
+
+       
+    }
+}
---
+++ Vezba25/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vezba25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Gradovi;
+            Console.Write("Unesite Grad>");
+            Gradovi = Console.ReadLine();
+            Console.Write("Unesite Grad>");
+             Gradovi= Console.ReadLine();
+            Console.Write("Unesite Grad>");
+            Gradovi = Console.ReadLine();
+            FileMenagment.WriteList(Gradovi);
+        }
+    }
+}
